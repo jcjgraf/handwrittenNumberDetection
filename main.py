dc39614a886d923002c368de892d4a76fe2a058a
Jean-Claude Graf <jeanggi90@gmail.com>
2018-01-12 00:21:11 +0100

    The coest function does not improve for some weird reason

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ import subprocess			# Clear Terminal
 root = None
 image = None
 
+neuralNetwork = None
+
 class Window(Frame):
 
 	def __init__(self, master=None):
@@ -63,7 +65,7 @@ class Menu:
 
 	def drawMenu():
 
-		subprocess.run( "clear")
+		# subprocess.run( "clear")
 		print("--------------")
 		print("     Menu     ")
 		print("--------------")
@@ -78,10 +80,9 @@ class Menu:
 			# Input menu
 
 			if getInputImage():
-				global image
-				# break
+				global image, neuralNetwork
 
-				neuralNetwork = NeuralNetwork()
+				# neuralNetwork = NeuralNetwork()
 
 				neuralNetwork.createNetwork()
 
@@ -91,18 +92,15 @@ class Menu:
 
 					sess.run(tf.global_variables_initializer())
 
-
-					# neuralNetwork.createNetwork()
-
 					result = neuralNetwork.feedToNetwork(neuralNetwork.x)
-
-					# sess.run(neuralNetwork.out, feed_dict={neuralNetwork.x: []})
 
 					foo = sess.run(neuralNetwork.output, feed_dict={neuralNetwork.x: [data]})
 
 					print("out", foo)
+					print("result", result)
+					print("resrun", sess.run(result))
 
-				sys.exit()
+				Menu.drawMenu()
 
 			else:
 				# Did not get image
@@ -110,7 +108,10 @@ class Menu:
 
 		elif i == 2:
 			# Train cnn
-			pass
+			neuralNetwork.createNetwork()
+			neuralNetwork.trainNetwork()
+
+			Menu.drawMenu()
 
 		elif i == 9:
 			# Exit
@@ -138,32 +139,70 @@ class NeuralNetwork:
 		self.hl1 = {"weights": tf.Variable(tf.random_normal([numNodesImp, numNodesHl1])),
 				"biases": tf.Variable(tf.random_normal([numNodesHl1]))}
 
-		self.hl2 = {"weights": tf.Variable(tf.random_normal([numNodesHl1, numNodesHl2])),
-				"biases": tf.Variable(tf.random_normal([numNodesHl2]))}
+		# self.hl2 = {"weights": tf.Variable(tf.random_normal([numNodesHl1, numNodesHl2])),
+		# 		"biases": tf.Variable(tf.random_normal([numNodesHl2]))}
 
-		self.hl3 = {"weights": tf.Variable(tf.random_normal([numNodesHl2, numNodesHl3])),
-				"biases": tf.Variable(tf.random_normal([numNodesHl3]))}
+		# self.hl3 = {"weights": tf.Variable(tf.random_normal([numNodesHl2, numNodesHl3])),
+		# 		"biases": tf.Variable(tf.random_normal([numNodesHl3]))}
 
-		self.out = {"weights": tf.Variable(tf.random_normal([numNodesHl3, numNodesOut])),
+		self.out = {"weights": tf.Variable(tf.random_normal([numNodesHl1, numNodesOut])),
 		"biases": tf.Variable(tf.random_normal([numNodesOut]))}
 
 	def feedToNetwork(self, data):
 
+		print("Evaluating input in cnn")
+
 		l1 = tf.add(tf.matmul(data, self.hl1["weights"]), self.hl1["biases"])
 		l1 = tf.nn.relu(l1)
 
-		l2 = tf.add(tf.matmul(l1, self.hl2["weights"]), self.hl2["biases"])
-		l2 = tf.nn.relu(l2)
+		# l2 = tf.add(tf.matmul(l1, self.hl2["weights"]), self.hl2["biases"])
+		# l2 = tf.nn.relu(l2)
 
-		l3 = tf.add(tf.matmul(l2, self.hl3["weights"]), self.hl3["biases"])
-		l3 = tf.nn.relu(l3)
+		# l3 = tf.add(tf.matmul(l2, self.hl3["weights"]), self.hl3["biases"])
+		# l3 = tf.nn.relu(l3)
 
-		self.output = tf.add(tf.matmul(l3, self.out["weights"]), self.out["biases"])
+		self.output = tf.add(tf.matmul(l1, self.out["weights"]), self.out["biases"])
 		self.output = tf.nn.relu(self.output)
 
-		# return output
+		return self.output
 
-	# def trainNetwork():
+	def trainNetwork(self):
+
+		print("Train neural network")
+
+		from tensorflow.examples.tutorials.mnist import input_data
+
+		mnist = input_data.read_data_sets("/temp/data/", one_hot=True)
+
+		y = tf.placeholder("float")
+		batch_size = 100
+
+		currentOut = neuralNetwork.feedToNetwork(neuralNetwork.x)
+
+		cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=currentOut, labels=y))
+		optimizer = tf.train.AdamOptimizer().minimize(cost)
+
+		numEpochs = 10
+		batchSize = 100
+
+		with tf.Session() as sess:
+
+			sess.run(tf.global_variables_initializer())
+
+			for epoch in range(numEpochs):
+				epochLoss = 0
+				for _ in range(int(mnist.train.num_examples / batchSize)):
+					epochX, epochY = mnist.train.next_batch(batchSize)
+
+					_, c = sess.run([optimizer, cost], feed_dict={neuralNetwork.x: epochX, y: epochY})
+					print("cost", c)
+					epochLoss += c
+
+				print("Epoch", epoch, "completed out of", numEpochs, "loss:", epochLoss)
+
+			correct = tf.equal(tf.argmax(currentOut, 1), tf.argmax(y, 1))
+			accuracy = tf.reduce_mean(tf.cast(correct, "float"))
+			print("Accuracy:", accuracy.eval({neuralNetwork.x: mnist.test.images, y: mnist.test.labels}))
 
 	def imageToMnist(self, image):
 		"""
@@ -181,7 +220,6 @@ class NeuralNetwork:
 			for column in row:
 				imageArray.append(float(column[0] / 255))
 
-		# print(imageArray)
 		return imageArray
 
 def getInputImage():
@@ -209,6 +247,8 @@ def getInputImage():
 		return False
 
 if __name__ == '__main__':
+
+	neuralNetwork = NeuralNetwork()
 
 	Menu.drawMenu()
 	
